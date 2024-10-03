3a7d4960ef9395084b414e40c366b39ff9d6bf43
Rohit Patel <rohitppttel@gmail.com>
2024-10-03 15:26:33 +0530

    random_joke_generator

---
+++ app.js
@@ -0,0 +1,37 @@
+const jokebtn = document.getElementById("Joke-btn");
+const setupDisplay = document.getElementById("setup");
+const Line = document.getElementById("Line");
+const loadingSpinner = document.getElementById("loading");
+
+async function Data() {
+    try {
+            // Show loading Animation
+            setupDisplay.textContent = "Loading...";
+            Line.textContent = "";
+            loadingSpinner.style.display = "block";
+            
+            const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+            const data = await response.json();
+            console.log(data);
+
+            // Hide the loading Animation
+            setupDisplay.textContent = data.setup;
+            Line.textContent = data.punchline; 
+            loadingSpinner.style.display = "none"; 
+        }
+        catch (error) {
+        setupDisplay.textContent = "Oops, couldn't fetch a joke";
+        Line.textContent = "";
+    } finally { 
+        loadingSpinner.style.display = "none"; 
+    }
+}
+
+jokebtn.addEventListener("click", () => {
+    setupDisplay.textContent = "Loading...";
+    Line.textContent = "";
+    loadingSpinner.style.display = "block";
+    setTimeout(Data, 500)
+}
+);
+
